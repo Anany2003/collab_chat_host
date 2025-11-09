f33f2279ba0191e432a9e706dcc289e0ec047a76
Ananya <ananychaturvedi2003@gmail.com>
2025-11-09 17:06:55 +0530

    sameSite set to "none" for now

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -56,7 +56,8 @@ export async function signup(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
+
       secure: process.env.NODE_ENV === "production",
     });
 
@@ -87,7 +88,7 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       secure: process.env.NODE_ENV === "production",
     });
 
